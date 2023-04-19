886069380b028b3854eaa39e94386afff7f1b728
Anna S <anna.sundberg@gritacademy.se>
2023-04-19 14:18:54 +0200

    routes

--- index.js
+++ index.js
@@ -6,20 +6,11 @@ const port = 3000;
 var mysql = require("mysql");
 //installerar (npm i) express-session för att kunna hantera vår session
 const session = require('express-session'); 
-
-//nedan rad för att vi ska kunna länka till css i html utan att länka via olika mappar.
-app.use(express.static("public"));
-
 //body-parser här nedan är inbyggt i express och gör om body till json
 let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//Set EJS as the view engine
-app.set('view engine', 'ejs');
-
-//specify the location of the of the view folder
-app.set('views', './views')
+require('dotenv').config();
 
 app.use(
     session({
@@ -28,6 +19,22 @@ app.use(
         saveUninitialized: true
     })
 );
+
+const connection = require('./db_connection');
+
+//ROUTING
+const router = require('./routes/routes')
+app.use(router); 
+
+
+//nedan rad för att vi ska kunna länka till css i html utan att länka via olika mappar.
+app.use(express.static("public"));
+
+//Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+//specify the location of the of the view folder
+app.set('views', './views')
 
 //allt man skriver i URL-fältet blir en get-request
 
@@ -39,94 +46,138 @@ app.use(
 //     res.send('Hello World!')
 // })
 
-app.get("/second", (req, res) => {
-    res.send("Hello Second World!");
-});
+// app.get("/second", (req, res) => {
+//     res.send("Hello Second World!");
+// });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+//lägger in i routes.js 
+// app.get("/", (req, res) => {
+//     const data = {
+//         title: "Welcome",
+//         style: "color: lightpink",
+//         // href: "./views/html/login.html"
+//         href: "/login"
+//     }
+//     //filen heter index
+//     res.render('index', data);
 
-app.get("/", (req, res) => {
-    const data = {
-        title: "Welcome",
-        style: "color: lightpink",
-        // href: "./views/html/login.html"
-        href: "/login"
-    }
-    //filen heter index
-    res.render('index', data);
+//     // res.sendFile(__dirname + "/views/html/index.html");
+// });
 
-    // res.sendFile(__dirname + "/views/html/index.html");
-});
+// app.get('/api/getuser', (req, res) => {
+//     res.json('{"name": "Anna"}');
+// }) vi byter detta till att hämta favoritecolor istället:
 
-app.get('/api/getuser', (req, res) => {
-    res.json('{"name": "Anna"}');
-})
 
-app.get('/logged-in', (req, res) =>{
-    if(req.session.authenticated){
-        // res.sendFile(__dirname + "/views/html/logged-in.html");
-        const data = {
-            name: 'Anna',
-            style: 'color: peachpuff'
-        }
+// app.get('/logged-in', (req, res) =>{
+//     if(req.session.authenticated){
+//         const username = req.session.username;
+//         const favoriteColor = req.session.favorite_color;
+//         // res.sendFile(__dirname + "/views/html/logged-in.html");
+//         const data = {
+//             // name: 'Anna',
+//             name: username,
+//             // style: 'color: peachpuff'
+//             style: `color: ${favoriteColor}`,
+//         }
 
-        res.render('logged-in', data)
-        }
+//         res.render('logged-in', data)
+//         }
         
-        else{
-            res.redirect('/login');
-        }
-})
+//         else{
+//             res.redirect('/login');
+//         }
+// })
 
-app.get("/login", (req, res) => {
-    console.log(req.body);
-    res.sendFile(__dirname + "/views/html/login.html");
-});
-
-//i package.json under scripts skriver vi bara olika kommandon som förenklar för oss. T ex "start": "nodemon index.js" gör att vi använder vår dependency nodemon som uppdaterar vår kod hela tiden utan att vi behöver cancel och run igen. Man kan även använda det om man t ex vill kompilera flera ccs filer osv.
-
-//databas connection. Nu har vi kört npm install mysql för att göra detta
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "admin",
-    password: "password",
-    database: "express-demo",
-});
-
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-
-    console.log("connected as id " + connection.threadId);
-});
-
-app.post("/login", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
+app.get('/api/getFavoriteColor', (req, res) => {
+    // res.json(`{"color": '${favoriteColor}'}`);
+if(req.session.authenticated && req.session.username)
+{
     connection.query(
-        ` SELECT * FROM users WHERE email = '${email}' AND password= ${password} `,
+
+        ` SELECT * FROM users WHERE name = '${req.session.username}'`,
         function (error, results, fields) {
             if (error) throw error;
 
             if (results.length > 0 ){
-                //res.send(`Found ${results.length} user`);
-                req.session.authenticated = true;
-                res.redirect('/logged-in');
+                console.log(results[0].favorite_color);
+                res.json(`{"color": ${results[0].favorite_color}}`);
             }
-
-            else{
-                res.send('No users found')
-            }
-            console.log(results);
         }
     );
+}
+else{
+    res.redirect('/login'); 
+}
 
-    // res.send('Got a POST request'); 
+})
 
+//i package.json under scripts skriver vi bara olika kommandon som förenklar för oss. T ex "start": "nodemon index.js" gör att vi använder vår dependency nodemon som uppdaterar vår kod hela tiden utan att vi behöver cancel och run igen. Man kan även använda det om man t ex vill kompilera flera ccs filer osv.
+
+//databas connection. Nu har vi kört npm install mysql för att göra detta
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 8889,
+//     user: "admin",
+//     password: "password",
+//     database: "express-demo",
+// }); vi kommenterar bort detta för att kunna hosta 
+
+
+// app.post("/login", (req, res) => {
+//     const email = req.body.email;
+//     const password = req.body.password;
+
+//     connection.query(
+//         ` SELECT * FROM users WHERE email = '${email}' AND password= ${password} `,
+//         function (error, results, fields) {
+//             if (error) throw error;
+
+//             if (results.length > 0 ){
+//                 console.log(results[0].name);
+//                 //res.send(`Found ${results.length} user`);
+//                 req.session.username = results[0].name;
+//                 req.session.favorite_color = results[0].favorite_color;
+//                 req.session.authenticated = true;
+//                 res.redirect('/logged-in');
+//             }
+
+//             else{
+//                 res.send('No users found')
+//             }
+//             console.log(results);
+//         }
+//     );
+
+//     // res.send('Got a POST request'); 
+
+// });
+
+
+// // SIGN UP
+// app.get('/signup', (req, res) => {
+//     res.render('signup')
+// })
+
+// // Route for creating a new user
+// app.post('/users', (req, res) => {
+//     const { name, email, password, favorite_color } = req.body;
+//     const user = { name, email, password, favorite_color };
+
+//     // Insert new user into MySQL database
+//     connection.query('INSERT INTO users SET ?', user, (err, results) => {
+//         if (err) {
+//             console.error('Error creating new user: ', err);
+//             res.status(500).send('Error creating new user');
+//             return;
+//         }
+//         console.log('New user created with id: ', results.insertId);
+//         req.session.username = user.name;
+//         req.session.authenticated = true;
+//         res.redirect('/logged-in');
+//     });
+// });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
---
+++ routes/routes.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const router = express.Router();
+const connection = require('../db_connection');
+require('dotenv').config();
+
+router.get("/", (req, res) => {
+    const data = {
+        title: "Welcome",
+        style: "color: lightpink",
+        // href: "./views/html/login.html"
+        href: "/login",
+        favCol: favoriteColor
+    }
+    //filen heter index
+    res.render('index', data);
+
+    // res.sendFile(__dirname + "/views/html/index.html");
+});
+
+module.exports = router;
+
+router.get('/logged-in', (req, res) =>{
+    if(req.session.authenticated){
+        const username = req.session.username;
+        const favoriteColor = req.session.favorite_color;
+        // res.sendFile(__dirname + "/views/html/logged-in.html");
+        const data = {
+            // name: 'Anna',
+            name: username,
+            // style: 'color: peachpuff'
+            style: `color: ${favoriteColor}`,
+        }
+
+        res.render('logged-in', data)
+        }
+        
+        else{
+            res.redirect('/login');
+        }
+})
+
+router.post("/login", (req, res) => {
+    console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    connection.query(
+        ` SELECT * FROM users WHERE email = '${email}' AND password= ${password} `,
+        function (error, results, fields) {
+            if (error) throw error;
+
+            if (results.length > 0 ){
+                console.log(results[0].name);
+                console.log(req.session);
+                //res.send(`Found ${results.length} user`);
+                req.session.username = results[0].name;
+                req.session.favorite_color = results[0].favorite_color;
+                req.session.authenticated = true;
+                res.redirect('/logged-in');
+            }
+
+            else{
+                res.send('No users found')
+            }
+            console.log(results);
+        }
+    );
+
+});
+
+router.get("/login", (req, res) => {
+    console.log(req.body);
+res.render('login'); 
+
+});
+
+// SIGN UP
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+
+// Route for creating a new user
+router.post('/users', (req, res) => {
+    const { name, email, password, favorite_color } = req.body;
+    const user = { name, email, password, favorite_color };
+
+    // Insert new user into MySQL database
+    connection.query('INSERT INTO users SET ?', user, (err, results) => {
+        if (err) {
+            console.error('Error creating new user: ', err);
+            res.status(500).send('Error creating new user');
+            return;
+        }
+        console.log('New user created with id: ', results.insertId);
+        req.session.username = user.name;
+        req.session.authenticated = true;
+        res.redirect('/logged-in');
+    });
+});
+
+
+router.get("/second", (req, res) => {
+    res.send("Hello Second World!");
+});
+
